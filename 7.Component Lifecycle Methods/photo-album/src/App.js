cee980a702ac30e459bd67e01811596a6f134d6c
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2023-08-14 20:24:04 +0530

    photo album added here

--- 7.Component Lifecycle Methods/photo-album/src/App.js
+++ 7.Component Lifecycle Methods/photo-album/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ImageList from "./components/ImageList";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+      imageUrl: ""
+    };
+  }
+
+  // Function to add image to local storage
+  addImageToLS = () => {
+    localStorage.setItem(
+      "images",
+      JSON.stringify([...this.state.images, this.state.imageUrl])
+    );
+  };
+
+  // Function to get images from the local storage
+  getImagesFromLS = () => {
+    const images = localStorage.getItem("images");
+    if (!images) {
+      localStorage.setItem("images", []);
+      return [];
+    }
+    return JSON.parse(images);
+  };
+
+  onAddImage = (e) => {
+    e.preventDefault();
+    this.addImageToLS();
+    this.setState((prevState) => {
+      return {
+        images: [...prevState.images, this.state.imageUrl],
+        imageUrl: ""
+      };
+    });
+  };
+  
+
+  // Create function to set image url
+  setImageUrl = (e) => {
+    this.setState({ imageUrl: e.target.value });
+  };
+  // Use appropriate lifecycle method to get images
+  componentDidMount() {
+    const images = this.getImagesFromLS();
+    this.setState({ images });
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.onAddImage}>
+          <input
+            type="text"
+            placeholder="Image URL"
+            onChange={this.setImageUrl}
+            value={this.state.imageUrl}
+          />
+          <button>Add Image</button>
+        </form>
+        <ImageList images={this.state.images} />
+      </>
+    );
+  }
 }
-
-export default App;
